7c402349c3b1995792bb2983584fce91cfe59195
manuconcepbrito <manu041196@gmail.com>
2020-06-13 19:41:26 +0100

    Add login route with email and password

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -23,3 +23,36 @@ exports.register = asyncHandler(async (req, res, next) => {
     data: user,
   });
 });
+
+// @desc  Login user
+//@route  POST /api/v1/auth/login
+//@access Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Check for missing fields
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide an email and password", 400));
+  }
+  // Check for user
+  // password is not returned by default then add it with `select`
+  const user = await User.findOne({ email }).select("+password");
+
+  // Important: Use same error for no user in db and incorrect password
+  // for security reasons.
+  // 401: Unauthorized
+  if (!user) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+
+  // Check if entered password is correct
+  const isPassword = await user.matchPassword(password);
+
+  if (!isPassword) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+  res.status(200).json({
+    success: true,
+    data: user.email,
+  });
+});
--- backend/models/User.js
+++ backend/models/User.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
-const isDoctor = function() {
+const isDoctor = function () {
   return this.role === "doctor";
-}
+};
 
 const UserSchema = new mongoose.Schema({
   firstname: {
@@ -40,7 +40,7 @@ const UserSchema = new mongoose.Schema({
   },
   experience: {
     type: String,
-    validate: [isDoctor, "Only Doctors can have field `experience`"]
+    validate: [isDoctor, "Only Doctors can have field `experience`"],
   },
   resetPasswordToken: String,
   resetPasswordExpire: Date,
@@ -51,9 +51,8 @@ const UserSchema = new mongoose.Schema({
 });
 
 // Mongoose middleware
-
 // Encrypt password in database
-UserSchema.pre("save", async function(next) {
+UserSchema.pre("save", async function (next) {
   // Higher the round the more secure but heavy
   // 10 is the recommended in the docs
   if (!this.isModified("password")) {
@@ -62,7 +61,10 @@ UserSchema.pre("save", async function(next) {
   const round = 10;
   const salt = await bcrypt.genSalt(round);
   this.password = await bcrypt.hash(this.password, salt);
-})
+});
 
-
+// Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function (inputPassword) {
+  return await bcrypt.compare(inputPassword, this.password);
+};
 module.exports = mongoose.model("User", UserSchema);

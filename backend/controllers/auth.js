5faeeebbcbeecd7029a8407282704236431cf9a0
manuconcepbrito <manu041196@gmail.com>
2020-06-14 18:10:48 +0100

    Add get current user route

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -54,6 +54,17 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc  Get current logged in user
+//@route  POST /api/v1/auth/me
+//@access Private
+exports.getCurrentUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
 // @desc  Update user details
 //@route  PUT /api/v1/auth/details
 //@access Private
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,6 +7,7 @@ const {
   updateDetails,
   resetPassword,
   forgotpassword,
+  getCurrentUser
 } = require("../controllers/auth");
 
 const router = express.Router();
@@ -14,6 +15,7 @@ const { protectRoute } = require("../middleware/auth");
 
 router.route("/register").post(register);
 router.route("/login").post(login);
+router.route("/me").get(protectRoute, getCurrentUser);
 router.route("/password").put(protectRoute, updatePassword);
 router.route("/details").put(protectRoute, updateDetails);
 router.route("/forgot-password").post(forgotpassword);

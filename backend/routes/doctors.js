82698d3307579de8014daba450a3c8a2f13a7323
manuconcepbrito <manu041196@gmail.com>
2020-06-16 10:42:41 +0100

    Search doctors within distance from user address

--- backend/routes/doctors.js
+++ backend/routes/doctors.js
@@ -4,6 +4,7 @@ const {
   getDoctors,
   getDoctor,
   addDoctorToHospital,
+  getDoctorsWithinRadius
 } = require("../controllers/doctors");
 
 // Preserve parameters from parent router (Hospital) when needed
@@ -17,5 +18,6 @@ router
   .post(protectRoute,authorize('doctor', 'admin'), addDoctorToHospital);
 
 router.route("/:id").get(getDoctor);
+router.route("/search/:distance").get(protectRoute, authorize('user'), getDoctorsWithinRadius)
 
 module.exports = router;
